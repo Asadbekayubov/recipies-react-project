7ba0205fcce159913aa004e4568a8c3b6302a487
Asadulloh <ayubovasadbek33@gmail.com>
2023-10-10 08:45:41 +0500

    changed committed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="bg-neutral-content">
+    <nav className="bg-neutral-content">
       <div className="align-elements flex justify-between items-center py-3">
         <h1 className="text-3xl">
           <Link to="/">Recipie</Link>
         </h1>
         <Link className="btn btn-outline-primary" to="/create">Create</Link>
       </div>
-    </div>
+    </nav>
   )
 }
 
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function Create() {
+  const [title, setTitle] = useState("");
+  const [cookingTime, setCookingTime] = useState(0);
+  const [img, setImg] = useState("");
+  const [method, setMethod] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const recipie = {
+      id: uuidv4(),
+      title: title,
+      cookingTime: `${cookingTime} minutes`,
+      img: img,
+      method: method,
+    };
+    console.log(recipie);
+  };
+
   return (
     <div>
       <h1 className="text-3xl text-center">Create New Recipie</h1>
-      <form className="flex flex-col items-center">
+      <form onSubmit={handleSubmit} className="flex flex-col items-center">
         <div className="form-control w-full max-w-xs mt-3">
           <label className="label">
             <span className="label-text">Title:</span>
           </label>
           <input
+            onChange={(e) => setTitle(e.target.value)}
             type="text"
             placeholder="Type here"
             className="input input-bordered w-full max-w-xs"
+            value={title}
           />
         </div>
         <div className="form-control w-full max-w-xs mt-3">
@@ -36,9 +56,11 @@ function Create() {
             <span className="label-text">Cooking Time :</span>
           </label>
           <input
+            onChange={(e) => setCookingTime(e.target.value)}
             type="text"
             placeholder="Type here"
             className="input input-bordered w-full max-w-xs"
+            value={cookingTime}
           />
         </div>
         <div className="form-control w-full max-w-xs mt-3">
@@ -46,9 +68,11 @@ function Create() {
             <span className="label-text">Image Url:</span>
           </label>
           <input
+            onChange={(e) => setImg(e.target.value)}
             type="url"
             placeholder="Put image url"
             className="textarea input-bordered w-full max-w-xs"
+            value={img}
           />
         </div>
         <div className="form-control w-full max-w-xs mt-3">
@@ -56,9 +80,11 @@ function Create() {
             <span className="label-text">Method:</span>
           </label>
           <textarea
+            onChange={(e) => setMethod(e.target.value)}
             type="textarea"
             placeholder="Type here"
             className="textarea input-bordered w-full max-w-xs"
+            value={method}
           />
         </div>
         <button className="btn btn-primary w-[320px] mt-5">Create</button>
